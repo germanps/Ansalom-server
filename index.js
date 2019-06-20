0da67f0ba988d464ddc209a92019f416df4f5bd3
germanps <gpla@pasiona.com>
2019-06-20 08:44:19 +0200

    verify token on server

--- index.js
+++ index.js
@@ -3,10 +3,35 @@ import express from 'express';
 import {ApolloServer} from 'apollo-server-express';
 import { typeDefs } from './data/schema';
 import { resolvers } from './data/resolvers';
+import dotenv from 'dotenv';
+dotenv.config({path: 'variables.env'});
+import jwt from 'jsonwebtoken';
 
 
 const app = express();
-const server = new ApolloServer({typeDefs, resolvers});
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async({req}) => {
+        //obtener token
+        const token = req.headers['authorization'];
+        if(token !== "null"){
+            try {
+                //verificar el token del frontend
+                const usuarioActual = await jwt.verify(token, process.env.SECRETO);
+                //agregar el usuario actual al request
+                req.usuarioActual = usuarioActual;
+                return {
+                    usuarioActual
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        
+    }
+    
+});
 
 server.applyMiddleware({app});
 
